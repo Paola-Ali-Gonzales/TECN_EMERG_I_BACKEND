7e5201eb421bd3b8f1022b43c09265e30db87bbc
Paola-Ali-Gonzales <agp2017905@est.univalle.edu>
2022-12-11 09:12:08 -0400

    delete

--- microservicios-almacenes/src/main/java/com/prpymes/microservicios/app/almacenes/models/repository/IProductImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prpymes.microservicios.app.almacenes.models.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.prpymes.microservicios.app.almacenes.models.entity.ProductImage;
-
-public interface IProductImageRepository extends CrudRepository<ProductImage, Long> {
-	
-}
--- microservicios-almacenes/src/main/java/com/prpymes/microservicios/app/almacenes/services/IProductImageService.java
+++
@@ -1,8 +0,0 @@
-package com.prpymes.microservicios.app.almacenes.services;
-
-import com.prpymes.microservicios.app.almacenes.models.entity.ProductImage;
-import com.prpymes.microservicios.commons.services.CommonService;
-
-public interface IProductImageService extends CommonService<ProductImage> {
-
-}
